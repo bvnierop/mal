babdaa728e8b6f783e6d65274f9a54b5d9db582f
Bart van Nierop <mail@bvnierop.nl>
2021-07-12 23:17:08 +0200

    Add step 0

---
+++ impls/csharp.bvn/step0_repl/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace step0_repl
+{
+    class Program
+    {
+        static string READ(string s)
+        {
+            return s;
+        }
+
+        static string EVAL(string s)
+        {
+            return s;
+        }
+
+        static string PRINT(string s)
+        {
+            return s;
+        }
+
+        static string rep(string line)
+        {
+            return PRINT(EVAL(READ(line)));
+        }
+
+        static void Main(string[] args)
+        {
+
+            string line = "";
+            do
+            {
+                Console.Write("user> ");
+                line = Console.ReadLine();
+                if (line != null) {
+                    Console.WriteLine(rep(line));
+                }
+            } while (line != null);
+        }
+    }
+}
