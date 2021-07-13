81b50741be83327bdcfb9c1bf8a76913a9da0f5e
Bart van Nierop <mail@bvnierop.nl>
2021-07-13 11:07:10 +0200

    Start step 1

---
+++ impls/csharp.bvn/step1_read_print/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace step0_repl
+{
+    class Program
+    {
+        static string READ(string s)
+        {
+            return s;
+        }
+
+        static string EVAL(string s)
+        {
+            return s;
+        }
+
+        static string PRINT(string s)
+        {
+            return s;
+        }
+
+        static string rep(string line)
+        {
+            return PRINT(EVAL(READ(line)));
+        }
+
+        static void Main(string[] args)
+        {
+            string line = "";
+            while ((line = ReadLine.Read("user> ")) != null)
+            {
+                Console.WriteLine(rep(line));
+            }
+        }
+    }
+}
